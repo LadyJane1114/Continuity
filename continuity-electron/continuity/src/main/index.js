cf71c7007e47fdac77945bdf9b7183e19e680815
Lady Jane <sweet.jane.s@outlook.com>
2026-02-06 10:25:10 -0400

    Fixed menu and added exit button

--- continuity-electron/continuity/src/main/index.js
+++ continuity-electron/continuity/src/main/index.js
@@ -72,5 +72,10 @@ app.on('window-all-closed', () => {
   }
 })
 
+// Quit the app on request from renderer
+ipcMain.on('app:quit', () => {
+  app.quit();
+});
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
